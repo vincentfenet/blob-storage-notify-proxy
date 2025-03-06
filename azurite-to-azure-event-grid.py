628f87b5afde6491019d733ccce7c6002dba2bed
Vincent FENET <vincent.fenet@fr.enersys.com>
2025-03-06 10:55:30 +0100

    add check

--- azurite-to-azure-event-grid.py
+++ azurite-to-azure-event-grid.py
@@ -17,28 +17,29 @@ def transform(data):
             and item["response"]["status_code"] == 201
         ):
             parts = item["request"]["path"].strip("/").split("/", maxsplit=2)
-            res.append(
-                {
-                    "topic": "",
-                    "subject": "",
-                    "eventType": "Microsoft.Storage.BlobCreated",
-                    "id": "",
-                    "data": {
-                        "api": "PutBlob",
-                        "requestId": "",
-                        "eTag": "",
-                        "contentType": "application/octet-stream",
-                        "contentLength": 16,
-                        "blobType": "BlockBlob",
-                        "accessTier": "Default",
-                        "url": f"http://{parts[0]}.blob.local:{CONFIG_PROXY_PORT}/{parts[1]}/"
-                        + parts[2].split("?")[0],
-                        "sequencer": "",
-                        "storageDiagnostics": {"batchId": ""},
-                    },
-                    "dataVersion": "",
-                    "metadataVersion": "1",
-                    "eventTime": datetime.datetime.now().isoformat() + "Z",
-                }
-            )
+            if len(parts) == 3:
+                res.append(
+                    {
+                        "topic": "",
+                        "subject": "",
+                        "eventType": "Microsoft.Storage.BlobCreated",
+                        "id": "",
+                        "data": {
+                            "api": "PutBlob",
+                            "requestId": "",
+                            "eTag": "",
+                            "contentType": "application/octet-stream",
+                            "contentLength": 16,
+                            "blobType": "BlockBlob",
+                            "accessTier": "Default",
+                            "url": f"http://{parts[0]}.blob.local:{CONFIG_PROXY_PORT}/{parts[1]}/"
+                            + parts[2].split("?")[0],
+                            "sequencer": "",
+                            "storageDiagnostics": {"batchId": ""},
+                        },
+                        "dataVersion": "",
+                        "metadataVersion": "1",
+                        "eventTime": datetime.datetime.now().isoformat() + "Z",
+                    }
+                )
     return res
